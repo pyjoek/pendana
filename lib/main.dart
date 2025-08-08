de6f0c112510dab3273b9b6bf1a380ffe875809f
pyjoek <joelnrlson@gmail.com>
2025-08-08 21:53:15 +0300

    the first sign up page

--- lib/main.dart
+++ lib/main.dart
@@ -42,21 +42,78 @@ class _MyAppState extends State<MyApp> {
     FlutterNativeSplash.remove(); // Remove native splash screen
   }
 
+  final Color background = Color(0xFFFFF9F6);
+  double width = 0;
+  double height = 0;
+
   @override
   Widget build(BuildContext context) {
+    width = MediaQuery.of(context).size.width;
+    height = MediaQuery.of(context).size.height;
+
     return MaterialApp(
-      title: 'Pendana',
-      theme: ThemeData(
-        primarySwatch: Colors.pink,
-      ),
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Pendana - Home'),
-        ),
-        body: Center(
-          child: Text(
-            'Welcome to Pendana ❤️',
-            style: TextStyle(fontSize: 24),
+        backgroundColor:background,
+        body: Padding(
+          padding: const EdgeInsets.all(28.0),
+          child: Center(
+            child: Column(
+              children: [
+                SizedBox(height: 120,),
+                Text("Create", style: TextStyle(
+                  fontWeight: FontWeight.w600, fontSize: 64
+                ),),
+                Text("Account", style: TextStyle(
+                  fontWeight: FontWeight.w600, fontSize: 64
+                ),),
+                SizedBox(height: 30,),
+                Text("Sign up to rind", style: TextStyle(
+                  fontWeight: FontWeight.w300, fontSize: 34
+                ),),
+                Text("Your match!", style: TextStyle(
+                  fontWeight: FontWeight.w300, fontSize: 34
+                ),),
+                SizedBox(height: 50,),
+                SizedBox(
+                    width: width * 0.7,
+                  child: TextFormField(
+                  decoration: InputDecoration(
+                    hintText: 'Name',
+                    filled: true,
+                    fillColor: background,
+                    contentPadding: EdgeInsets.symmetric(vertical: 15, horizontal: 20),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30),
+                    )
+                  ),
+                ),
+                ),
+                SizedBox(height: 20,),
+                InkWell(
+                  onTap: () => {print('signing up')},
+                  child: Container(
+                    child: Center(child: Text("sign up", style: TextStyle(
+                      fontWeight: FontWeight.w600, fontSize: 30, color: Colors.white
+                    ))),
+                    width: width * 0.7,
+                    height: 50,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(30),
+                      color: Color(0xFFE28A89)
+                    ),
+                  ),
+                ),
+              SizedBox(height: height * 0.3,),
+              InkWell(
+                onTap: () => {print('logging....')},
+                child: Text("Log in", style: TextStyle(
+                    fontWeight: FontWeight.w300, fontSize: 30, color: Color(0xFFE28A89)
+                  )
+                ),
+              )
+              ],
+            ),
           ),
         ),
       ),
