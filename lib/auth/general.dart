81e9459c5720aa1bd1e3a21cd0d536ba8507f74e
pyjoek <joelnrlson@gmail.com>
2025-08-21 21:12:42 +0300

    fixing ui

--- lib/auth/general.dart
+++ lib/auth/general.dart
@@ -31,7 +31,8 @@ class _GeneralState extends State<General> {
   Set<String> selectedLikes = {};
   DateTime? selectedDate;
   String purpose = '';
-  final addr = "10.0.2.2:8000/api";
+  // final addr = "10.0.2.2:8000/api";
+  final addr = "127.0.0.1:8000/api";
   bool _loading = false;
 
   void validateAndNext() {
